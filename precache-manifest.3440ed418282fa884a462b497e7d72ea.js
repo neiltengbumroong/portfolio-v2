73db1973477d0ee2446401f6b4c8eec075a60068
Neil Tengbumroong <tengneil99@gmail.com>
2020-07-14 23:29:08 -0700

    Updates

---
+++ precache-manifest.3440ed418282fa884a462b497e7d72ea.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e09ecea921439761d7be3b0252820cb9",
+    "url": "/portfolio-v2/index.html"
+  },
+  {
+    "revision": "de20b4db910922a7c3fb",
+    "url": "/portfolio-v2/static/css/main.eb0cbd4c.chunk.css"
+  },
+  {
+    "revision": "52f4a7a2ccbba421dd15",
+    "url": "/portfolio-v2/static/js/2.5f2466ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-v2/static/js/2.5f2466ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de20b4db910922a7c3fb",
+    "url": "/portfolio-v2/static/js/main.20071607.chunk.js"
+  },
+  {
+    "revision": "58daaf2044f28a9eccfa",
+    "url": "/portfolio-v2/static/js/runtime-main.a8d6ddc0.js"
+  },
+  {
+    "revision": "46cdb38630e8f7ea5ee8519658ee51c5",
+    "url": "/portfolio-v2/static/media/Cabin-Regular.46cdb386.ttf"
+  },
+  {
+    "revision": "251a6aa3f84c7062bc2584ec7ea19e70",
+    "url": "/portfolio-v2/static/media/NeilTengbumroong.251a6aa3.pdf"
+  },
+  {
+    "revision": "d2a8929f630cba5875d97a5f34da6162",
+    "url": "/portfolio-v2/static/media/Raleway-ExtraLight.d2a8929f.ttf"
+  }
+]);
